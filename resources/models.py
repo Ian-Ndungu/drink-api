0f176681f19579ba5da4378114c11bab51ed36af
njagah <ianndungu06@gmail.com>
2024-07-22 14:54:25 +0300

    returning

--- resources/models.py
+++ resources/models.py
@@ -10,13 +10,3 @@ class Drink(db.Model):
     image_url = db.Column(db.String(200), nullable=False)
     price = db.Column(db.Float, nullable=False)
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(200), nullable=False)
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
